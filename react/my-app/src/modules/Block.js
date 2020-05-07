97f6ab7091aec46da51cf9cd6eb8945b5b807dd1
krakendebil <koshmakvl@gmail.com>
2020-05-07 18:40:54 +0300

    It works!

--- react/my-app/src/modules/Block.js
+++ react/my-app/src/modules/Block.js
@@ -23,7 +23,17 @@ export default class Block extends React.Component {
 	};
 
 	upload = () => {
-		alert(this.state.text);
+		var filename = 'Uploaded-file';
+		var element = document.createElement('a');
+	  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.state.text));
+	  element.setAttribute('download', filename);
+
+	  element.style.display = 'none';
+	  document.body.appendChild(element);
+
+	  element.click();
+
+	  document.body.removeChild(element);
 	};
 
 	rendNorm = () => {
