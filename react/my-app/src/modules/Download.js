06ec1b0b3f919686fc3ea8a7f31ce3d1465cf34f
Vladyslav Herasymchuk <slaughternal@gmail.com>
2020-05-29 20:30:16 +0300

    Maked pdf-lib variant

--- react/my-app/src/modules/Download.js
+++ react/my-app/src/modules/Download.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from "react-bootstrap"
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
 
 export default class Download extends React.Component {
   constructor(props) {
@@ -26,9 +27,14 @@ export default class Download extends React.Component {
     const selectedFile = document.getElementById('inputFile').files[0];
 
     const reader = new FileReader
-    reader.readAsDataURL(selectedFile);
-    reader.onload = function (e) {
-      console.log(reader.result)
+    reader.readAsArrayBuffer(selectedFile);
+    reader.onload = async function (e) {
+      const pdfDoc = await PDFDocument.load(reader.result)
+      const firstPage = pdfDoc.getPage(0)
+      console.log(firstPage)
+      console.log(firstPage.getContentStream().computeContents())
+      console.log(Utf8ArrayToStr(firstPage.getContentStream().computeContents()))
+      console.log(firstPage.getSize())
     };
 
     reader.onerror = function (e) {
@@ -48,6 +54,39 @@ export default class Download extends React.Component {
     </Card>
   )
 
+  Utf8ArrayToStr = (array) => {
+    var out, i, len, c;
+    var char2, char3;
+    console.log(array)
+    out = "";
+    len = array.length;
+    i = 0;
+    while (i < len) {
+      c = array[i++];
+      switch (c >> 4) {
+        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+          // 0xxxxxxx
+          out += String.fromCharCode(c);
+          break;
+        case 12: case 13:
+          // 110x xxxx   10xx xxxx
+          char2 = array[i++];
+          out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+          break;
+        case 14:
+          // 1110 xxxx  10xx xxxx  10xx xxxx
+          char2 = array[i++];
+          char3 = array[i++];
+          out += String.fromCharCode(((c & 0x0F) << 12) |
+            ((char2 & 0x3F) << 6) |
+            ((char3 & 0x3F) << 0));
+          break;
+      }
+    }
+
+    return out;
+  }
+
   render() {
     let results
 
@@ -59,4 +98,37 @@ export default class Download extends React.Component {
 
     return results
   }
+}
+
+function Utf8ArrayToStr(array) {
+  var out, i, len, c;
+  var char2, char3;
+  console.log(array)
+  out = "";
+  len = array.length;
+  i = 0;
+  while (i < len) {
+    c = array[i++];
+    switch (c >> 4) {
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12: case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+          ((char2 & 0x3F) << 6) |
+          ((char3 & 0x3F) << 0));
+        break;
+    }
+  }
+
+  return out;
 }
