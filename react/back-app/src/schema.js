21d296d2e881a5580e4dadc3dfac72d96060d7e1
Vladyslav Herasymchuk <slaughternal@gmail.com>
2020-06-02 20:04:02 +0300

    Added graphql server part

---
+++ react/back-app/src/schema.js
@@ -0,0 +1,35 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Mutation {
+  post(url: String!, description: String!): Link!
+  signup(email: String!, password: String!, name: String!): AuthPayload
+  login(email: String!, password: String!): AuthPayload
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+  postedBy: User
+}
+
+type AuthPayload {
+  token: String
+  user: User
+}
+
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  links: [Link!]!
+}
+`;
+
+module.exports = typeDefs;
